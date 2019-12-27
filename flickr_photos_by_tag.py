4e61c575564a880c3c4afce6cfc84dca8f17ac48
Sehail Fillali <sehail.fillali@gmail.com>
2019-12-27 17:07:44 -0500

    refactored; added cmdline arguments support; impoved logic

--- flickr_photos_by_tag.py
+++ flickr_photos_by_tag.py
@@ -1,4 +1,6 @@
-import time
+import argparse, textwrap, time, json, re, requests, random, datetime
+from datetime import datetime as dt
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -6,19 +8,94 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import json, itertools, re, requests, random, datetime
-from datetime import datetime as dt
-from fake_useragent import UserAgent
 
-options = Options()
+#from fake_useragent import UserAgent
+parser = argparse.ArgumentParser(description=textwrap.dedent('''\
+        scrape JSONs containing photos from flickr
+         '''))
 
-options.add_argument('--headless')
+parser.add_argument("-a", "--after_date", help="Start date (in unix timestamp format). Defaults to yesterday", type=float, default=int(dt.now().timestamp()) - (24 * 3600))
+parser.add_argument("-b", "--before_date", help="End date (in unix timestamp format). Defaults to now", type=float, default=int(dt.now().timestamp()))
+parser.add_argument("-s", "--courtesy_sleep", help="Range (in string format) from which a random value will be chosen to sleep randomly. example: '1.3, 2.7'", type=str, default="1.3, 2.7")
+parser.add_argument("-n", "--photos_per_page", help="Photos per file. Default is 500 which is the maximum", type=int, default=500)
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="count", default=0)
+parser.add_argument("-t", "--test", help="test mode", action='store_true')
+parser.add_argument("-p", "--dump_path", help="Path where to dump json files", type=str, default='./json_data/')
+parser.add_argument("-x", "--add_extras", help="extra json fields to request. Defaults to 'url_o,original_format,date_taken,date_upload,geo'", type=str, default="url_o,original_format,date_taken,date_upload,geo")
+
+args = parser.parse_args()
+
+# verbose = args.verbose
+# test = args.test
+# AFTER_DATE = args.after_date
+# BEFORE_DATE = args.before_date
+# PHOTOS_PER_PAGE = args.photos_per_page
+# additional_extras = args.add_extras
+# DATA_PATH = args.dump_path
+
+# print(args)
+
+
+for each in args.__dict__: globals()[each.upper()] = args.__dict__[each]
+
+COURTESY_SLEEP = [int(COURTESY_SLEEP.split(',')[0]) , int(COURTESY_SLEEP.split(',')[1])]
+
+DATA_PATH = ('./test/' if TEST else DATA_PATH)
+
+
+# VERBOSE = 3
+
+# TEST = 1
+
+# AFTER_DATE = 1576471428
+
+# BEFORE_DATE = dt.now()
+
+# PHOTOS_PER_PAGE = 500
+
+# ADD_EXTRAS = "url_o,original_format,date_taken,date_upload,geo"
+
+
+params = {
+    "sort" : "relevance",
+    "parse_tags" : "1",
+    "content_type" : "7",
+    "lang": "en-US",
+    "has_geo" :"1",
+    "media" : "photos",
+    "view_all" : "1",
+    "text" : "clouds",
+    "viewerNSID": "",
+    "method" : "flickr.photos.search",
+    "csrf" : "",
+    "format" : "json",
+    "hermes" : "1",
+    "hermesClient" : "1",
+    "nojsoncallback" : "1",
+    "geo_context": '2', # 0: all , 1: indoors, 2 : outdoors
+    "privacy_filter" : 1
+}
+
+privacy_filters = '''
+"public photos" : '1',
+"private photos visible to friends" : '2',
+"private photos visible to family" : '3',
+"private photos visible to friends & family": '4',
+"completely private photos" : '5'
+'''
 
 FLICKR = 'https://flickr.com/search/'
 TAGS = ['clouds', 'cloud', 'sky', 'storm', 'weather']
 
+options = Options()
+
+headless = True
+if headless: options.add_argument('--headless')
+
 caps = DesiredCapabilities.CHROME
 caps['loggingPref'] = {'performance': 'ALL'}
+
+driver = webdriver.Chrome(options = options, desired_capabilities=caps)
 
 xhrCallIntercept_js = """
 (function(XHR) {
@@ -64,29 +141,26 @@ xhrCallIntercept_js = """
 })(XMLHttpRequest);
 """
 
-driver = webdriver.Chrome(options = options, desired_capabilities=caps)
-
 try:
     url = FLICKR + "?has_geo=1&media=photos&view_all=1&text=" + TAGS[0]
 
     driver.get(url)
+
     driver.execute_script(xhrCallIntercept_js)
 
     print('title : "{}"'.format(driver.title))
 
-    time.sleep(5)
-
 except Exception as e:
-    print('Error: ' + str(e))
+    print('Error! Cannot open search page: ' + str(e))
 
-wait = "Getting AJAX data..."
+wait = False
 while wait != True:
-    print(wait)
+    if VERBOSE >= 1: print("Getting AJAX data...")
     # trying scroll to trigger and api call
     try:
-        print('attempting Scroll!')
+        if VERBOSE >= 3: print('attempting Scroll!')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        print('Scrolled...')
+        if VERBOSE >= 3: print('Scrolled...')
 
         # waiting for the api call to be included in the DOM
         wait = WebDriverWait(driver, 15).until(EC.text_to_be_present_in_element((By.ID, "interceptedResponse"), "api_key"))
@@ -96,11 +170,14 @@ while wait != True:
 
     intercepts = driver.find_elements_by_id('interceptedResponse')
 
-if wait == True:
-    print('ajax call intercepted!\n')
-
+if wait == True and VERBOSE >= 1: print('ajax call intercepted!\n')
 xhr_call = intercepts[0].text
 
+cookies = driver.get_cookies()
+driver.close()
+
+
+# pasrsing data from DOM element
 var_names = ["api_key", "reqId", "api_url", "extras"]
 re_expressions = [r"(api_key)=([\dabcdef]*)(&)", r"(reqId)=([\dabcdef]*)(&)", r"(https:\/\/(\w+\.?)+(\/\w+)+)(\?)", r"extras=((\w+(%2)?)+?)?&"]
 
@@ -108,6 +185,7 @@ groups = [2,2,1,1]
 
 variables = [dict(zip(["var_name", "regex", 'group'], each)) for each in [each for each in zip(var_names, re_expressions, groups)]]
 
+#creating variables for each parsed data
 for each in variables:
 
     if  re.search(each["regex"], string=xhr_call, flags=re.MULTILINE) != None:
@@ -117,66 +195,33 @@ for each in variables:
 
 extras = extras.replace('%2C', ',')
 
-print("Extracted ajax params:--------\n")
-for each in var_names:
-    print("%(var)s :     %(value)s" % {"var": each.ljust(10, ' '), "value" : globals()[each]})
+if VERBOSE >= 2:
+    print("Extracted ajax params:--------\n")
+    for each in var_names:
+        print("%(var)s :     %(value)s" % {"var": each.ljust(10, ' '), "value" : globals()[each]})
 
-
-photos_per_page = "500"
-additional_extras = "url_o,original_format,date_taken,date_upload,geo"
-
-privacy_filter ={
-    "public photos" : '1',
-    "private photos visible to friends" : '2',
-    "private photos visible to family" : '3',
-    "private photos visible to friends & family": '4',
-    "completely private photos" : '5'
-}
-
-
-
-global params
-params = {
-    "sort" : "relevance",
-    "tags" : 'clouds',
-    "parse_tags" : "1",
-    "content_type" : "7",
-    "extras" : extras + additional_extras,
-    "per_page" : photos_per_page,
-    "page" : 1,
-    "lang": "en-US",
-    "has_geo" :"1",
-    "media" : "photos",
-    "view_all" : "1",
-    "text" : "clouds",
-    "viewerNSID": "",
-    "method" : "flickr.photos.search",
-    "csrf" : "",
+added_params = {
+    "extras" : extras + ADD_EXTRAS,
+    "per_page" : PHOTOS_PER_PAGE,
     "api_key" : api_key,
-    "format" : "json",
-    "hermes" : "1",
-    "hermesClient" : "1",
-    "reqId" : reqId,
-    "nojsoncallback" : "1",
-    "privacy_filter" : privacy_filter['public photos'],
-    "geo_context": '2'
+    "reqId" : reqId
 }
 
-cookies = driver.get_cookies()
+for each in added_params: params[each] = added_params[each]
 
-#ua = UserAgent()
-driver.close()
+spoof_webdriver = False
+
+if spoof_webdriver: ua = UserAgent()
 
 with requests.sessions.Session() as s:
     for cookie in cookies:
         s.cookies.set(cookie['name'], cookie['value'])
-    #s.headers['User-Agent'] = str(ua.chrome)
-    response = s.get(api_url, params=params)
+    if spoof_webdriver: s.headers['User-Agent'] = str(ua.chrome)
+response = s.get(api_url, params=params)
 
-json_data_path = './json_data/'
-photo_json = 'photos.json'
-scraped_ids = 'scraped_photos.txt'
-now = dt.now()
+
+
+now = BEFORE_DATE
 days_offset = 3
 
 def change_date_range(index, offset=3):
@@ -185,7 +230,7 @@ def change_date_range(index, offset=3):
 
 change_date_range(now)
 
-params['tags'] = ''
+
 
 total_photos = s.get(api_url, params=params).json()['photos']['total']
 
@@ -193,68 +238,88 @@ def find_best_date_range():
 
     global days_offset, total_photos
 
-    print(f"Finding a better range (in 20 attempts or less) ...")
+    params['per_page'] = 1
+
+    if VERBOSE >=3: print(f"Finding a better range (in 20 attempts or less) ...")
     repeats = 0
     #int(total_photos) <= 3990 or int(total_photos) > 4000
     while (int(total_photos) != 4000) and not (int(total_photos) < 4000 and repeats > 20):
         if int(total_photos) > 4000:
             days_offset = days_offset * 4000/ int(total_photos)
-            print(f"({str(repeats)}): too many photos   ({total_photos.ljust(10, '+')}): new range from {params['min_upload_date']} to {params['max_upload_date']}", end = '\r')
+            if VERBOSE >=3: print(f"({str(repeats)}): too many photos ({total_photos.ljust(5, '+')}): new range from {params['min_upload_date']} to {params['max_upload_date']}", end = '\r')
         if int(total_photos) <= 3990:
             days_offset = days_offset * 3990 / int(total_photos)
-            print(f"({str(repeats)}): not enough photos ({total_photos.ljust(10, '-')}): new range from {params['min_upload_date']} to {params['max_upload_date']}", end = '\r')
+            if VERBOSE >=3: print(f"({str(repeats)}): not enough photos ({total_photos.ljust(5, '-')}): new range from {params['min_upload_date']} to {params['max_upload_date']}", end = '\r')
         params['min_upload_date'] = params['max_upload_date'] - datetime.timedelta(days_offset)
-
-        params['per_page'] = 1
         total_photos = s.get(api_url, params=params).json()['photos']['total']
         repeats += 1
+    params['per_page'] = PHOTOS_PER_PAGE
 
+def write_each_page_as_json_file(path, params, session, max_pages):
+    params['per_page'] = PHOTOS_PER_PAGE
+
+    for page in range(1, 1 + max_pages):
+        print(f"Requesting page {page}...".ljust(120, ' '),  end='\r')
+
+        params['page'] = page
+
+
+        before = dt.now().timestamp()
+        try:
+            response = session.get(api_url, params=params)
+        except Exception as e:
+            print(f"Couldn't request JSON data:____ {e}")
+        after = dt.now().timestamp()
+
+        last_response_time = after - before
+
+
+        # Trying to write JSON data to file
+        file_to_be_written = f"{path}{term}_{str(params['min_upload_date'].timestamp())}-{str(params['max_upload_date'].timestamp())}_{page}.json"
+        #if VERBOSE >=3: print(f" file path to be written: {file_to_be_written}\n\n")
+        try:
+            with open(file_to_be_written, 'w') as outfile:
+                json.dump(response.json(), outfile)
+                #print('\r', end = '')
+            print(f"{file_to_be_written} written succesfully!")
+        except Exception as e:
+            print(f"problem dumping json data: {str(e)}")
+        if VERBOSE >=2: print(f'sleeping for {last_response_time * random.randint(COURTESY_SLEEP[0], COURTESY_SLEEP[1])} seconds... (for courtesy :))'.ljust(120, ' '), end = '\r')
+        time.sleep(last_response_time * random.randint(COURTESY_SLEEP[0], COURTESY_SLEEP[1]))
+
+
+last_response_time = 0
 for term in TAGS:
 
     params['text'] = term
 
     first_range = True
+    response = s.get(api_url, params=params)
+
     while params['min_upload_date'].timestamp() >= 1483228800 :
         if not first_range:
             change_date_range(params['max_upload_date'] - datetime.timedelta(days=days_offset), days_offset)
+            total_photos = response.json()['photos']['total']
         else:
             first_range = False
-        total_photos = s.get(api_url, params=params).json()['photos']['total']
-        print("__________________________")
-        print(f"New date range: {params['min_upload_date']} to {params['max_upload_date']}______ total photos : {total_photos}")
+            total_photos = s.get(api_url, params=params).json()['photos']['total']
+        print("".ljust(120, '_'))
+        if VERBOSE >=3: print(f"New date range: {params['min_upload_date']} to {params['max_upload_date']}")
 
         total_photos = s.get(api_url, params=params).json()['photos']['total']
-        print(f"Photos in current range: {total_photos}")
+        if VERBOSE >=3: print(f"Photos in current range: {total_photos}")
 
         find_best_date_range()
-        total_photos = s.get(api_url, params=params).json()['photos']['total']
-        print(f"Better range: {params['min_upload_date']} to {params['max_upload_date']}______ total photos : {total_photos}".ljust(200, ' '))
+
+        response = s.get(api_url, params=params)
+        total_photos = response.json()['photos']['total']
+        if VERBOSE >= 2: print(f"Next suitable range: {params['min_upload_date']} to {params['max_upload_date']}______ total photos : {total_photos}".ljust(120, ' '))
 
         print(f'starting JSON dump...')
+        max_pages = response.json()['photos']['max_allowed_pages']
 
-        for page in range(1, 9):
-            print(f"getting page {page}...", end = ' ')
+        write_each_page_as_json_file(path=DATA_PATH, params=params, session=s, max_pages=max_pages)
 
-            params['page'] = page
-            params['per_page'] = photos_per_page
+        time.sleep(last_response_time * random.uniform(COURTESY_SLEEP[0], COURTESY_SLEEP[1]) * 2)
 
-            before = dt.now().timestamp()
-
-            response = s.get(api_url, params=params)
-
-            after = dt.now().timestamp()
-
-            response_time = after - before
-            print(f'sleeping {response_time * random.randint(2, 6)} seconds... (for courtesy :))', end = '\r')
-            time.sleep(response_time * random.randint(0, 3))
-
-            path = f"{json_data_path}{term}_{str(params['min_upload_date'].timestamp())}-{str(params['max_upload_date'].timestamp())}_{page}.json"
-            try:
-                with open(path, 'w') as outfile:
-                    json.dump(response.json(), outfile)
-                print(f"{path} written succesfully!")
-            except Exception as e:
-                print(f"problem dumping json data: {str(e)}")
-        time.sleep(random.randrange(1000, 2500)/ 1000)
-
-    time.sleep(random.randrange(4000, 7000)/ 1000)
+    time.sleep(random.uniform(COURTESY_SLEEP[0], COURTESY_SLEEP[1]) * 3)
